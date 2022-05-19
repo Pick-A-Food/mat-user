df0b0a0a1744fe2a312843763977cdc1d31dba1b
GormWH <sp4kid@gmail.com>
2022-05-19 20:00:23 +0900

    initial commit

---
+++ src/main/java/com/matjo/pickafood/user/MatUserApplication.java
@@ -0,0 +1,13 @@
+package com.matjo.pickafood.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatUserApplication.class, args);
+	}
+
+}
